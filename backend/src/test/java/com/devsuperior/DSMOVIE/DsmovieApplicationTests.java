1274e657a39c89c38862a0ba1b8b4262549ef83a
Tati Geronutti <tatiane_g1985@hotmail.com>
2024-08-21 01:04:57 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devsuperior/DSMOVIE/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSMOVIE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
